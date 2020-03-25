7069d297051d3d2a567a7f6c8531f92e0b2664f2
huangchengdu <huang2009303513@gmail.com>
2020-03-25 18:05:41 +0800

    react-redux connect3.0已经完成。

--- to-react-redux/src/components/Counter.js
+++ to-react-redux/src/components/Counter.js
@@ -1,35 +1,46 @@
 import React, { Component } from "react";
 import { bindActionCreators } from "../redux";
-import store from '../store';
-import actions from '../store/actions/counter';
-import { connect } from '../react-redux';
+import store from "../store";
+import actions from "../store/actions/counter";
+import { connect } from "../react-redux";
 
 const bindCounter = bindActionCreators(actions, store.dispatch);
 
 class Counter extends Component {
-    render() {
-        return (
-            <div>
-                <p>{`number: ${this.props.number}`}</p>
-                <button onClick={() => { this.props.add(2) }}>+</button>
-                <button onClick={() => { this.props.minus(2) }}>-</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <p>{`number: ${this.props.number}`}</p>
+        <button
+          onClick={() => {
+            this.props.add(2);
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            this.props.minus(2);
+          }}
+        >
+          -
+        </button>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    number: state.counter.number
+  number: state.counter.number
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    add: (num) => {
-        dispatch(actions.add(num))
-    },
-    minus: (num) => {
-        dispatch(actions.minus(num))
-    }
+const mapDispatchToProps = dispatch => ({
+  add: num => {
+    dispatch(actions.add(num));
+  },
+  minus: num => {
+    dispatch(actions.minus(num));
+  }
 });
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- to-react-redux/src/react-redux/components/connect.js
+++ to-react-redux/src/react-redux/components/connect.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import store from '../../store';
+import { bindActionCreators } from '../../redux';
+import { ReactReduxContext } from "./Context";
+import shallowEqual from '../utils/shallowEqual';
 
-export default function connect(mapStateToProps, mapDispatchToProps) {
+
+const defaultMapStateToProps = state => ({});
+const defaultMapDispatchToProps = dispatch => ({});
+
+export default function connect(mapStateToProps = defaultMapStateToProps, mapDispatchToProps = defaultMapDispatchToProps) {
     return function wrapWithConnect(WrappedConnect) {
         return class Connect extends Component {
+            static contextType = ReactReduxContext;
             constructor(props, context) {
                 super(props, context);
-                // this.store = context.store;
-                this.store = store;
-                this.state = mapStateToProps(this.store.getState());
-                this.mappedDispatch = mapDispatchToProps(this.store.dispatch);
+                this.store = context.store;
+                // this.state = mapStateToProps(this.store.getState());
+                // this.mappedDispatch = mapDispatchToProps(this.store.dispatch);
+
+                //源码中是将 store.getState() 给了 this.state
+                this.state = mapStateToProps(this.store.getState(), this.props);
+                if (typeof mapDispatchToProps === 'function') {
+                    this.mappedDispatch = mapDispatchToProps(this.store.dispatch, this.props);
+                } else {
+                    //传递了一个 actionCreator 对象过来
+                    this.mappedDispatch = bindActionCreators(mapDispatchToProps, this.store.dispatch);
+                }
+                
             }
 
             componentDidMount() {
                 this.unsub = this.store.subscribe(() => {
-                    const mappedState = mapStateToProps(this.store.getState());
+                    const mappedState = mapStateToProps(this.store.getState(), this.props);
+                    if (shallowEqual(this.state, mappedState)) {
+                        return;
+                    }
                     this.setState(mappedState);
                 });
             }
